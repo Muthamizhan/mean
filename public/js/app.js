8688ba5121f329da338679003b415f7abac43fbb
Muthamizhan <msmuthu008@gmail.com>
2015-07-13 12:33:49 +0530

    get method dummy data

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var app = angular.module('MyApp', ['ngMaterial'])
+var app = angular.module('MyApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope','$http', function($scope,$http){
+	$http.get('/contactlist').success(function(response){
+		console.log(response);
+		$scope.contactList = response;
+	});
+}]);
+
--- server.js
+++ server.js
@@ -5,7 +5,29 @@ var app = express();
 	res.send("Hello World from server.js")
 });*/
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
+
+app.get('/contactlist', function(request, response) {
+    console.log("hi get");
+    var data = [{
+        "name": "Muthu",
+        "email": "muthu@email.com",
+        "Number": "111 111 222"
+    }, {
+        "name": "Thamizh",
+        "email": "thamizh@email.com",
+        "Number": "222 222 222"
+    }, {
+        "name": "Ms",
+        "email": "ms@email.com",
+        "Number": "33 333 333"
+    }, {
+        "name": "MSD",
+        "email": "msd@email.com",
+        "Number": "444 44 444"
+    }];
+    response.json(data);
+});
 
 app.listen(3000);
-console.log("hi ");
+console.log("localhost:3000 ");
